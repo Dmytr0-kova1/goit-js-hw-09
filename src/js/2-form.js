cb4303b1f90b0ef9ccab43e3bb3fed3fcbedaa9f
Dmytro Koval <kdmytro793@gmail.com>
2024-10-20 19:55:38 +0300

    hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+
+const form = document.querySelector(".feedback-form");
+const formData = {
+    email: "",
+    message: ""
+};
+
+
+
+function updateFormData({}) {
+  formData.email = document.querySelector("input").value;
+  formData.message = document.querySelector("textarea").value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
+form.addEventListener("input", updateFormData);
+
+const savedFormData = localStorage.getItem("feedback-form-state");
+if (savedFormData) {
+  const parsedData = JSON.parse(savedFormData);
+  document.querySelector("input").value = parsedData.email;
+  document.querySelector("textarea").value = parsedData.message;
+  formData = parsedData;
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+  } else {
+    console.log("The form has been sent:", formData);
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+  }
+});
